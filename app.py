7716ae69be13b2991c7313f6be82d8efe5f1a01a
unknown <kartiksaini@gmail.com>
2020-11-24 02:20:58 +0530

    REST API - payment-stripe

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import os
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','postgres+psycopg2://postgres:1234@localhost:5432/REST API - stores & items')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'key'
 api = Api(app)
